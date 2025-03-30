52511f7dda5cf30083daa3223a67dfc3d712a92b
amir-mc <amirfooladi93@gmail.com>
2025-03-30 12:01:43 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   reactCompiler: true,
+  //   ppr: 'incremental'
+  // },
 };
 
 export default nextConfig;
